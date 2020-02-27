cc8b145a88a02f9f51473ec76de70f4ba44b7799
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-02-27 16:33:59 +0100

    Added testSelectAllToggling
    Added test for checking singal emission after changing state of selectAll checkbox.

--- tests/FilterNamesTest.cpp
+++ tests/FilterNamesTest.cpp
@@ -8,16 +8,35 @@
 
 #include "FilterNames.h"
 
+void FilterNamesTest::initTestCase()
+{
+    testEntriesList_ = QStringList{"a", "b", "c", "d"};
+}
+
 void FilterNamesTest::testToggling()
 {
-    FilterNames filter(" ", {"a", "b", "c", "d"});
+    FilterNames filter(" ", testEntriesList_);
     auto listWidget = filter.findChild<QListWidget*>();
     auto selectAll = filter.findChild<QCheckBox*>();
     QCOMPARE(listWidget->isEnabled(), true);
     QCOMPARE(selectAll->isEnabled(), true);
 
     filter.setChecked(false);
-
     QCOMPARE(listWidget->isEnabled(), false);
     QCOMPARE(selectAll->isEnabled(), false);
+}
+
+void FilterNamesTest::testSelectAllToggling()
+{
+    FilterNames filter(" ", testEntriesList_);
+    QSignalSpy spy(&filter, &FilterNames::newStringFilter);
+
+    auto selectAll = filter.findChild<QCheckBox*>();
+    selectAll->toggle();
+    QCOMPARE(spy.count(), SIGNAL_RECEIVED);
+    QCOMPARE(spy.takeFirst(), {QVariant(testEntriesList_)});
+
+    selectAll->toggle();
+    QCOMPARE(spy.count(), SIGNAL_RECEIVED);
+    QCOMPARE(spy.takeFirst(), {QVariant(QStringList{})});
 }
--- tests/FilterNamesTest.h
+++ tests/FilterNamesTest.h
@@ -2,12 +2,23 @@
 #define FILTERNAMESTEST_H
 
 #include <QObject>
+#include <QStringList>
 
 class FilterNamesTest : public QObject
 {
     Q_OBJECT
 private slots:
+    void initTestCase();
+
     void testToggling();
+
+    void testSelectAllToggling();
+
+private:
+    QStringList testEntriesList_;
+
+    static constexpr int NO_SIGNAL {0};
+    static constexpr int SIGNAL_RECEIVED {1};
 };
 
 #endif // FILTERNAMESTEST_H
