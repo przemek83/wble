167d461a9c021345c850f910ca7800a4cd001eda
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-03-07 17:31:30 +0100

    Added testChangingEditLinesValues
    Added testtChangingEditLinesValues to FilterIntegersTest class.

--- tests/FilterIntegersTest.cpp
+++ tests/FilterIntegersTest.cpp
@@ -6,11 +6,13 @@
 #include <QtTest/QtTest>
 #include <QTest>
 
+#include "Utilities.h"
+
 #include "FilterIntegers.h"
 
 void FilterIntegersTest::testToggling()
 {
-    FilterIntegers filter("", 0., 100.);
+    FilterIntegers filter("", fromValue_, toValue_);
     auto lineEdits = filter.findChildren<QLineEdit*>();
     for (const auto& lineEdit : lineEdits)
         QCOMPARE(lineEdit->isEnabled(), true);
@@ -18,4 +20,39 @@ void FilterIntegersTest::testToggling()
     filter.setChecked(false);
     for (const auto& lineEdit : lineEdits)
         QCOMPARE(lineEdit->isEnabled(), false);
+}
+
+std::tuple<QLineEdit*, QLineEdit*>
+FilterIntegersTest::getLineEdits(QList<QLineEdit*> lineEdits) const
+{
+    const int firstValue =
+        QLocale::system().toInt(lineEdits.first()->text());
+    QLineEdit* fromEdit = (firstValue == fromValue_ ?
+                           lineEdits.first() : lineEdits.last());
+    QLineEdit* toEdit = (firstValue == toValue_ ?
+                         lineEdits.first() : lineEdits.last());
+    return {fromEdit, toEdit};
+}
+
+void FilterIntegersTest::testChangingEditLinesValues()
+{
+    FilterIntegers filter("", fromValue_, toValue_);
+    auto lineEdits = filter.findChildren<QLineEdit*>();
+    Q_ASSERT(lineEdits.count() == 2);
+    auto [fromLineEdit, toLineEdit] = getLineEdits(lineEdits);
+
+    QSignalSpy spy(&filter, &FilterIntegers::newNumericFilter);
+    fromLineEdit->setText("20");
+    QTest::keyClick(fromLineEdit, Qt::Key_Enter);
+    QApplication::processEvents();
+    QCOMPARE(spy.count(), SIGNAL_RECEIVED);
+    QList<QVariant> expectedValues {20, 100};
+    QCOMPARE(spy.takeFirst(), expectedValues);
+
+    toLineEdit->setText("50");
+    QTest::keyClick(fromLineEdit, Qt::Key_Enter);
+    QApplication::processEvents();
+    QCOMPARE(spy.count(), SIGNAL_RECEIVED);
+    expectedValues = {20, 50};
+    QCOMPARE(spy.takeFirst(), expectedValues);
 }
--- tests/FilterIntegersTest.h
+++ tests/FilterIntegersTest.h
@@ -1,7 +1,10 @@
 #ifndef FILTERINTEGERSTEST_H
 #define FILTERINTEGERSTEST_H
 
+#include <QList>
 #include <QObject>
+
+class QLineEdit;
 
 class FilterIntegersTest: public QObject
 {
@@ -9,7 +12,14 @@ class FilterIntegersTest: public QObject
 private slots:
     void testToggling();
 
+    void testChangingEditLinesValues();
+
 private:
+    std::tuple<QLineEdit*, QLineEdit*> getLineEdits(QList<QLineEdit*> lineEdits) const;
+
+    static constexpr int fromValue_ {0};
+    static constexpr int toValue_ {100};
+
     static constexpr int NO_SIGNAL {0};
     static constexpr int SIGNAL_RECEIVED {1};
 };
