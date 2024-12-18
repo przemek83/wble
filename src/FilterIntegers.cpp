c33a4854a6c0132acf607b0e0f1c47ee3cd2b6bb
przemek83 <4788832+przemek83@users.noreply.github.com>
2024-12-18 17:31:24 +0100

    Add missing constness.

--- src/FilterIntegers.cpp
+++ src/FilterIntegers.cpp
@@ -12,8 +12,10 @@ FilterIntegers::FilterIntegers(const QString& name, double from, double to,
 
     const int fromInt{static_cast<int>(from)};
     const int toInt{static_cast<int>(to)};
-    QValidator* fromValidator = new QIntValidator(fromInt, toInt, fromLineEdit);
-    QValidator* toValidator = new QIntValidator(fromInt, toInt, toLineEdit);
+    const QValidator* fromValidator =
+        new QIntValidator(fromInt, toInt, fromLineEdit);
+    const QValidator* toValidator =
+        new QIntValidator(fromInt, toInt, toLineEdit);
     fromLineEdit->setValidator(fromValidator);
     toLineEdit->setValidator(toValidator);
 
@@ -25,8 +27,8 @@ bool FilterIntegers::isDoubleMode() const { return false; }
 
 void FilterIntegers::emitChangeSignal()
 {
-    QLineEdit* fromLineEdit = getFromLineEdit();
-    QLineEdit* toLineEdit = getToLineEdit();
+    const QLineEdit* fromLineEdit = getFromLineEdit();
+    const QLineEdit* toLineEdit = getToLineEdit();
 
     Q_EMIT newNumericFilter(QLocale::system().toInt(fromLineEdit->text()),
                             QLocale::system().toInt(toLineEdit->text()));
